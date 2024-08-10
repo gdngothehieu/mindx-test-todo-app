4d1c143d844e3eec494be468d5d29395ca78332c
Ngo The Hieu <cusaniv@gmail.com>
2024-08-10 10:55:11 +0700

    fixed bug

--- src/assets/components/List.jsx
+++ src/assets/components/List.jsx
@@ -1,40 +1,76 @@
 import React from "react";
 import "./Tabs.css";
-function List({ allList, status, checkBox, deleteItem, ...props }) {
+function List({ allList, status, checkBox, deleteItem, setAllList, ...props }) {
+    const savedList = JSON.parse(localStorage.getItem("allList"));
+  React.useEffect(() => {
+  }, []);
   return (
     <div>
-      {allList.map((todo, id) => {
-        if (todo.status === status || status === "All") {
-          return (
-            <div key={todo.title} className="flex">
-              <input
-                onChange={(e) => {
-                  checkBox(e, todo.title);
-                }}
-                value={todo.status}
-                type="checkbox"
-              />
-              <p
-                className="ml-5"
-                style={
-                  todo.isChecked ? { textDecoration: "line-through" } : null
-                }
-              >
-                {todo?.title}
-              </p>
-              {status === "Completed" ? (
-                <img
-                  className="trash-can"
-                  src="./public/trash-can.png"
-                  onClick={() => {
-                    deleteItem(todo.title);
-                  }}
-                />
-              ) : null}
-            </div>
-          );
-        }
-      })}
+      {savedList
+        ? JSON.parse(localStorage.getItem("allList")).map((todo, id) => {
+            if (todo.status === status || status === "All") {
+              return (
+                <div key={todo.title} className="flex">
+                  <input
+                    onChange={(e) => {
+                      checkBox(e, todo.title);
+                    }}
+                    value={todo.status}
+                    type="checkbox"
+                  />
+                  <p
+                    className="ml-5"
+                    style={
+                      todo.isChecked ? { textDecoration: "line-through" } : null
+                    }
+                  >
+                    {todo?.title}
+                  </p>
+                  {status === "Completed" ? (
+                    <img
+                      className="trash-can"
+                      src="./public/trash-can.png"
+                      onClick={() => {
+                        deleteItem(todo.title);
+                      }}
+                    />
+                  ) : null}
+                </div>
+              );
+            }
+          })
+        : allList.map((todo, id) => {
+            if (todo.status === status || status === "All") {
+              return (
+                <div key={todo.title} className="flex">
+                  <input
+                    onChange={(e) => {
+                      checkBox(e, todo.title);
+                    }}
+                    value={todo.status}
+                    type="checkbox"
+                  />
+                  <p
+                    className="ml-5"
+                    style={
+                      todo.isChecked ? { textDecoration: "line-through" } : null
+                    }
+                  >
+                    {todo?.title}
+                  </p>
+                  {status === "Completed" ? (
+                    <img
+                      className="trash-can"
+                      src="./public/trash-can.png"
+                      onClick={() => {
+                        deleteItem(todo.title);
+                      }}
+                    />
+                  ) : null}
+                </div>
+              );
+            }
+          })}
     </div>
   );
 }
